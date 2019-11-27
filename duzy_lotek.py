59945038b3a8dd7422f8ccbffa3583a09e26382a
pmachnik90 <57916098+pmachnik90@users.noreply.github.com>
2019-11-27 16:20:28 +0100

    1.2.4.2.
     Do 3 razy sztuka

--- duzy_lotek.py
+++ duzy_lotek.py
@@ -6,21 +6,25 @@ lista = []
 typy = set()
 i = 0
 j = 0
-while i < ileliczb:
-    typ = int(input("Podaj liczbę {}: ".format(i+1)))
-    if typ not in typy:
-        typy.add(typ)
-        i += 1
-print("Twoje typy to: ", typy)
 while j < ileliczb:
     liczba = random.randint(1,maksliczba)
     if lista.count(liczba) == 0:
         lista.append(liczba)
         j += 1
+for p in range(3):
+    while i < ileliczb:
+        typ = int(input("Podaj liczbę {}: ".format(i+1)))
+        if typ not in typy:
+            typy.add(typ)
+            i += 1
+    #print("Twoje typy to: ", typy)
+    trafione = set(lista) & typy
+    if trafione:
+        print("\nIlość trafień: {}".format(len(trafione)))
+        print("Trafione liczby: ", trafione)
+    else:
+        print("Brak trafień. Spróbuj jeszcze raz!")
+    print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+    i = 0
+    typy = set()
 print("Wylosowane liczby ", lista)
-trafione = set(lista) & typy
-if trafione:
-    print("\nIlość trafień: {}".format(len(trafione)))
-    print("Trafione liczby: ", trafione)
-else:
-    print("Brak trafień. Spróbuj jeszcze raz!")
